d5bac38fc3c34a908ee866a8fa6071c883f6d0af
mohiulislam <mohiulislam900@gmail.com>
2022-06-24 22:32:23 +0600

    first commit

---
+++ prac.js
@@ -0,0 +1,69 @@
+// var obj = {
+//     x: 1,
+//     y: 2,
+//     ab() {
+//         a=6;
+//         console.log(a);
+        
+//     }
+    
+// }
+// obj.ab();
+//fn();
+
+// function greting() {
+//     console.log(s);
+//     var s=5;
+// }
+// let func = function(a,b,c) {
+//     console.log(arguments);
+// }
+// console.log(s);
+
+// func();
+// var a=5;
+// const a;
+// console.log(a);
+// if (true) {
+   
+// }
+// function abc() {
+//     let b=5;
+// }
+// console.log(b);
+// const user = {
+//     firstname:'mohiul',
+//     lastname:'Islam',
+//     address:{
+//         city:'Jessore',
+//         country:'Bangladesh'
+//     },
+//     func: function () {
+//         console.log(arguments);
+//     }
+// }
+// let a= new String();
+// a="hi"
+// console.log(typeof a);
+// function a() {
+//     for (var i = 1; i<=5; i++) {
+//         setTimeout(function () {
+//             console.log(i);
+//         }, i*2000);
+//     }
+// }
+// a();
+$(document).ready(function(){
+
+
+
+
+    $("button").click(function(){
+      $("p").slideToggle();
+    });
+
+
+
+
+
+  });
